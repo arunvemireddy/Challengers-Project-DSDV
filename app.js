f24637934f5facd0d29aac5bc861bea6139162c5
arunvemireddy <vemireddyarun0701@gmail.com>
2021-11-11 22:21:14 -0700

    database connection

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
 const express = require("express");
 const app = express();
+const url = "mongodb://localhost:27017";
+const dbName = "Netflix_Database";
+const collectionName ="Netflix";
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+
 app.get('/',(req,res)=>res.send("Hello"));
 
-app.listen(3000,()=>console.log("Server is running"));
-
-const {MongoClient} = require("mongodb");
-
-async function main(){
-    const url = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
-    const client = new MongoClient(url);
-    try{
-        await client.connect();
-        await listDatabases(client);
-    }
-    catch(e){
-        console.error(e);
-    }
-    finally{
-        await client.close();
-    }
-}
-    main().catch(console.error);
-
-let database =[];
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
-    console.log("Databases:");
-    databasesList.databases.forEach(element => {
-        console.log(element.name);
-        database.push(element.name);
-    });
-}
-
-app.get("/getdatabase",(req,res)=>res.send(database));
+app.listen(3000,()=>{console.log("Server is running")
+client.connect(function (err){
+    if(err) throw err;
+    console.log("connected to db");
+    db = client.db(dbName);
+    collection = db.collection("Netflix");
+})
+});
